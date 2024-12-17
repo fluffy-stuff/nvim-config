5888a5f6f9b22ecdf04b2149a3f8248272368681
Yuki Takahashi <yuki.takahashi.dev@gmail.com>
2024-12-17 08:21:45 +0000

    create nvim config repository

---
+++ lua/config/core.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.laststatus = 2
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+map('i', 'jj', '<ESC>', opts)
---
+++ lua/plugins/core.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    lazy = false,
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      { "<leader>n", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+  },
+  { "folke/noice.nvim", lazy = false },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = {'plugins/config/lualine.lua'}
+  },
+  {
+    'github/copilot.vim',
+    lazy = false
+  }
+}
